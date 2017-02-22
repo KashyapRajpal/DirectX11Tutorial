41940919488d9e47abbf2aec23821871b661b54e
KashyapRajpal <kashyapdrajpal@gmail.com>
2017-02-21 23:26:58 -0800

    Draw a Square using Triangle Strip Topology
    - Changed Vertex and index count to 4 for square
    - Changed Topology to Triangle Strip

--- GraphicsEngine/ModelClass.cpp
+++ GraphicsEngine/ModelClass.cpp
@@ -71,10 +71,10 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 
 
 	// Set the number of vertices in the vertex array.
-	m_vertexCount = 3;
+	m_vertexCount = 4;
 
 	// Set the number of indices in the index array.
-	m_indexCount = 3;
+	m_indexCount = 4;
 
 	// Create the vertex array.
 	vertices = new VertexType[m_vertexCount];
@@ -92,19 +92,25 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 
 	// Load the vertex array with data.
 	vertices[0].position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);  // Bottom left.
-	vertices[0].color = D3DXVECTOR4(1.0f, 1.0f, 0.0f, 1.0f);
+	vertices[0].color = D3DXVECTOR4(1.0f, 0.0f, 0.0f, 1.0f);
 
-	vertices[1].position = D3DXVECTOR3(0.0f, 1.0f, 0.0f);  // Top middle.
-	vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 1.0f, 1.0f);
+	vertices[1].position = D3DXVECTOR3(-1.0f, 1.0f, 0.0f);  // Top Left.
+	vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
 
-	vertices[2].position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);  // Bottom right.
-	vertices[2].color = D3DXVECTOR4(1.0f, 1.0f, 0.0f, 1.0f);
+	vertices[2].position = D3DXVECTOR3(1.0f, 1.0f, 0.0f);  // Top right.
+	vertices[2].color = D3DXVECTOR4(0.0f, 0.0f, 1.0f, 1.0f);
+
+	vertices[3].position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);  // Bottom right.
+	vertices[3].color = D3DXVECTOR4(1.0f, 1.0f, 0.0f, 1.0f);
 
 	// Load the index array with data.
+
+	// Triangle Strip
 	indices[0] = 0;  // Bottom left.
 	indices[1] = 1;  // Top middle.
-	indices[2] = 2;  // Bottom right.
-
+	indices[2] = 3;  // Bottom Right
+	indices[3] = 2;  // Top right.
+	
 	// Set up the description of the static vertex buffer.
     vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
     vertexBufferDesc.ByteWidth = sizeof(VertexType) * m_vertexCount;
@@ -193,7 +199,7 @@ void ModelClass::RenderBuffers(ID3D11DeviceContext* deviceContext)
 	deviceContext->IASetIndexBuffer(m_pIndexBuffer, DXGI_FORMAT_R32_UINT, 0);
 
     // Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
-	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
 	return;
 }
